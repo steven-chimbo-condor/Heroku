5e5ace45c3bd63fd0d45965f64713fb3d63a333b
steven-chimbo-condor <stevenchimboc@gmail.com>
2020-02-05 16:00:06 -0500

    3

--- config_connection/connection.js
+++ config_connection/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var connection = mongoose.connect('mongodb+srv://Chimbomba:Loja1998@cluster0-aksgn.mongodb.net/test?retryWrites=true&w=majority');
+var connection = mongoose.connect('mongodb+srv://Chimbomba:Loja1998@cluster0-aksgn.mongodb.net/Usuario?retryWrites=true&w=majority');
 
 
 mongoose.connection.on('open',(ref)=>{
